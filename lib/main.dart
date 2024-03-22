19cbecad8a210249d5d6c29b03fcf7355f55f22f
dev-barro <bardra10@gmail.com>
2024-03-22 03:45:50 +0100

    tuto flutter SingleChildScrollView et routage

--- lib/main.dart
+++ lib/main.dart
@@ -3,68 +3,146 @@ import 'package:flutter/material.dart';
 void main() {
   runApp(myApp());
 }
+
 class myApp extends StatelessWidget {
   const myApp({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return  MaterialApp(
-      home: Scaffold(
-        appBar:AppBar(
+    return MaterialApp(
+      home: MyWidget(),
+    );
+  }
+}
+
+class MyWidget extends StatelessWidget {
+  const MyWidget({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
           backgroundColor: Theme.of(context).colorScheme.inversePrimary,
           title: Text('Homepage'),
         ),
-body:SingleChildScrollView(
-  child:  Column(
-  mainAxisAlignment: MainAxisAlignment.start,
-  crossAxisAlignment: CrossAxisAlignment.stretch,
-  children: [
-    Container(
-      height: 750,
-      width: 100,
-      padding: const EdgeInsets.all(16),
-      color: Colors.purple,
-      child:const  Center(
-        child: Text('1',
-        style: TextStyle(
-          fontSize: 30,fontWeight:FontWeight.bold,
-      
-        ),),
-      ),
-    ),
- 
-    Container(
-      height: 750,
-      width: 100,
-      padding: const EdgeInsets.all(16),
-      color: Colors.yellowAccent,
-      child:const  Center(
-        child: Text('2',
-        style: TextStyle(
-          fontSize: 30,fontWeight:FontWeight.bold,
-      
-        ),),
-      ),
-    ),
+        body: SingleChildScrollView(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.start,
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: [
+              Container(
+                height: 750,
+                width: 100,
+                padding: const EdgeInsets.all(16),
+                color: Colors.purple,
+                child: const Center(
+                  child: Text(
+                    '1',
+                    style: TextStyle(
+                      fontSize: 30,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                ),
+              ),
+              Container(
+                height: 750,
+                width: 100,
+                padding: const EdgeInsets.all(16),
+                color: Colors.yellowAccent,
+                child: const Center(
+                  child: Text(
+                    '2',
+                    style: TextStyle(
+                      fontSize: 30,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                ),
+              ),
+              Container(
+                height: 750,
+                width: 100,
+                padding: const EdgeInsets.all(16),
+                color: Colors.red,
+                child: const Center(
+                  child: Text(
+                    '2',
+                    style: TextStyle(
+                      fontSize: 30,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                ),
+              ),
+              Container(
+                child: ElevatedButton(
+                  onPressed: () {
+                    Navigator.push(
+                      context,
+                      MaterialPageRoute(builder: (context) => Mapage()),
+                    );
+                  },
+                  child: Text('Go to Second Page'),
+                ),
+              )
+            ],
+          ),
+        ));
+  }
+}
 
-       Container(
-      height: 750,
-      width: 100,
-      padding: const EdgeInsets.all(16),
-      color: Colors.red,
-      child:const  Center(
-        child: Text('2',
-        style: TextStyle(
-          fontSize: 30,fontWeight:FontWeight.bold,
-      
-        ),),
+class Mapage extends StatelessWidget {
+  const Mapage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.green,
+        title: Text('Deuxième Page'),
       ),
-    )
-  
-  
-  ],
-),
-)   ),
+      body: Column(
+        crossAxisAlignment: CrossAxisAlignment.stretch,
+        children: [
+          Expanded(
+            flex: 1,
+            child: Container(
+              color: Colors.red,
+              child: Center(
+                child: Text(
+                  'Page 2 - Conteneur 1',
+                  style: TextStyle(color: Colors.white),
+                ),
+              ),
+            ),
+          ),
+          Expanded(
+            flex: 3,
+            child: Container(
+              color: const Color.fromARGB(255, 54, 73, 244),
+              child: Center(
+                child: Text(
+                  'Page 2 - Conteneur 2',
+                  style: TextStyle(color: Colors.white),
+                ),
+              ),
+            ),
+          ),
+          Expanded(
+            flex: 3,
+            child: Container(
+              color: const Color.fromARGB(255, 54, 244, 244),
+              child: Center(
+                child: Text(
+                  'Page 2 - Conteneur 3',
+                  style: TextStyle(color: Colors.white),
+                ),
+              ),
+            ),
+          ),
+        ],
+      ),
     );
   }
 }
